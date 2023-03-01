6b1e5b4e66cbfbf48a46e277f4e3ab842da3ccd0
aalexff <alexfigueroahrdz@gmail.com>
2023-03-01 08:57:17 -0600

    v 1.0 alternativa
    Se cambiaron las matrices por unas 4x4
    Se refino el calculador de R
    Se mejoro el sistema de multiplicacion producto punto.
    Se sugiere el calculo de error.

--- main.c
+++ main.c
@@ -5,38 +5,42 @@
 
 //Primitivas de funciones
 float wAndX();
+float wOrX();
 bool calculoDeR(float r);
-/* Posibles matrices individuales de tamaño completo, 
-bool xOR[4][4]= {{0,0,0,1},{0,1,1,1},{1,0,1,1},{1,1,1,1}} //bool??
-bool xAND[4][4]= {{0,0,0,1},{0,1,0,1},{1,0,0,1},{1,1,1,1}}
 
-ojala pudiera dibujar en los IDE
-Matriz de OR
-0 0 1 1 Xentrada1
-0 1 0 1 Xentrada2
-0 1 1 1 Resultados
-1 1 1 1 La matriz que dijo Juan que siempre esta en 1
-
-Matriz de AND
-0 0 1 1 Xentrada1
-0 1 0 1 Xentrada2
-0 0 0 1 Resultados
-1 1 1 1 La matriz que dijo Juan que siempre esta en 1
-*/
 //Declaracion de parametros
 float n = 0.25, err;
 int iteracion = 0;
 bool x[4][2]={{0,0},{0,1},{1,0},{1,1}};
-int weight[2] = {0.8, 0.4};
+int weight[4] = {0.8, 0.4, 0.7, 0.1};
 
-int main(int argc, char *argv[]) {
+bool xOR[4][4]= {{0,0,0,1},{0,1,1,1},{1,0,1,1},{1,1,1,1}}; //bool??
+bool xAND[4][4]= {{0,0,0,1},{0,1,0,1},{1,0,0,1},{1,1,1,1}};
+	/*
+		Matriz de OR
+		0 0 1 1 Xentrada1
+		0 1 0 1 Xentrada2
+		0 1 1 1 Resultados
+		1 1 1 1 La matriz que dijo Juan que siempre esta en 1
+		
+		Matriz de AND
+		0 0 1 1 Xentrada1
+		0 1 0 1 Xentrada2
+		0 0 0 1 Resultados
+		1 1 1 1 La matriz que dijo Juan que siempre esta en 1
+		
+		Sin la fila de resultados, los pesos para OR y AND serian iguales,
+		pero ahora necesita un peso mas para hacer el producto punto.
+		Un total de 4 pesos 
+	*/
+
+int perceptronMain(int argc, char *argv[]) {
 	//Declaracion de variables
-	bool andl[4] = {0,0,0,1}, orl[4] = {0,1,1,1};
-
+	//bool andl[4] = {0,0,0,1}, orl[4] = {0,1,1,1};
 	while (iteracion <= 100){
 		wAndX();
-	/*	wOrX();
-		yAnd();
+		wOrX();
+	/*	yAnd();
 		yOr();
 		//LMS
 		w(n+1)And
@@ -48,19 +52,44 @@ int main(int argc, char *argv[]) {
 }
 
 float wAndX(){
-	float r[4];
+	float rAnd[4];
+	bool Fr;
 	for(int j = 0; j <= 3; j++){
-		for(int i = 0; i <= 1; i++){
-			r[j] = weight[i] * x[j][i];
-			calculoDeR(r[i]);
+		for(int i = 0; i <= 3; i++){ //Multiplicación producto punto, lee un peso por cada elemento en cada fila
+			rAnd[j] = weight[i]; //Para que no haya alteracion del valor del peso en la primera iteracion
+			weight[i] = weight[i] * xAND[j][i]; //El valor del peso cambia con dada elemento
+			/*Calculo de error, err = (dn??? - wXn)
+			calculo de peso w(n+1) = Wi - n*err*x[j][i] No se si esto ya cuente como la funcion w(n+1) en main.
+			modificar ↓↓↓↓↓*/			
+			weight[i] = weight[i-1]+weight[i]; //weight vale su valor actual mas el anterior.		
 		}	
+		Fr = calculoDeR(rAnd[j]); // No se que se hacia con el calculo de R pero estaba en el cuaderno
+	}
+}
+
+float wOrX(){
+	float rOr[4]; //= a resultado wXn
+	bool Fr;
+	for(int j = 0; j <= 3; j++){ //Multiplicación producto punto, lee un peso por cada elemento en cada fila
+		for(int i = 0; i <= 3; i++){ 
+			rOr[j] = weight[i]; //Para que no haya alteracion del valor del peso en la primera iteracion
+			weight[i] = weight[i] * xAND[j][i]; //El valor del peso cambia con dada elemento
+			/*Calculo de error, err = (dn??? - wXn)
+			calculo de peso w(n+1) = Wi - n*err*x[j][i]
+			modificar ↓↓↓↓↓*/
+			weight[i] = weight[i-1]+weight[i]; //weight vale su valor actual mas el anterior.	
+		}
+		Fr = calculoDeR(rOr[j]);	
 	}
 }
 
 bool calculoDeR(float r){
+	bool Fr;
 	if (r>=0){
-		return 1;
+		Fr = 1;
+		return Fr;
 	}else{
-		return 0;
+		Fr = 0;
+		return Fr;
 	}
 }
