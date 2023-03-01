4fb94c4b08fefbff57d1b34ebc4ddc138646313a
aalexff <alexfigueroahrdz@gmail.com>
2023-02-28 19:51:15 -0600

    v0.3
    Se cambiaron los pesos por valores en un arreglo para que todo sean matrices.
    Se cambio a r como por arreglo para guardar los resultados.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ bool calculoDeR(float r);
 float n = 0.25, err;
 int iteracion = 0;
 bool x[4][2]={{0,0},{0,1},{1,0},{1,1}};
-int weightOne = 0.5, weightTwo = 0.8;
+int weight[2] = {0.8, 0.4};
 
 int main(int argc, char *argv[]) {
 	//Declaracion de variables
@@ -31,11 +31,11 @@ int main(int argc, char *argv[]) {
 }
 
 float wAndX(){
-	float r;
+	float r[4];
 	for(int j = 0; j <= 3; j++){
 		for(int i = 0; i <= 1; i++){
-			r = weightOne * x[j][i];
-			calculoDeR(r);
+			r[j] = weight[i] * x[j][i];
+			calculoDeR(r[i]);
 		}	
 	}
 }
